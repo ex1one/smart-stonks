3e2c8de1b016466773c83541d04944fc73087de4
ex1one <vyvf11@gmail.com>
2023-03-19 18:41:47 +0500

    initialization of Blitz with Sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require("@sentry/nextjs")
+
 // @ts-check
-const {withBlitz} = require("@blitzjs/next")
+const { withBlitz } = require("@blitzjs/next")
 
 /**
  * @type {import('@blitzjs/next').BlitzConfig}
@@ -7,3 +13,5 @@ const {withBlitz} = require("@blitzjs/next")
 const config = {}
 
 module.exports = withBlitz(config)
+
+module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: true })
